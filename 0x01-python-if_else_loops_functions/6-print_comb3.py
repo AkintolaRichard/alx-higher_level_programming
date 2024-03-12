ff761090b83c8dbc12961036c343fd0f4ce8a837
AkintolaRichard <princerichard17a@gmail.com>
2024-03-13 00:54:31 +0100

    fix: Add changes to file 3 to 6

--- 0x01-python-if_else_loops_functions/6-print_comb3.py
+++ 0x01-python-if_else_loops_functions/6-print_comb3.py
@@ -2,7 +2,7 @@
 for i in range(9):
     for j in range(i + 1, 10):
         if (i == 0 and j == i + 1):
-            print(f"{i}{j}", end="")
+            print("{}{}".format(i, j), end="")
         else:
-            print(f", {i}{j}", end="")
+            print(", {}{}".format(i, j), end="")
 print()
