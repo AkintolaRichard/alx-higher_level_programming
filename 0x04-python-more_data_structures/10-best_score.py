bb73175a2741168387d4794d05e785249ae9091a
AkintolaRichard <princerichard17a@gmail.com>
2024-03-22 11:09:37 +0100

    fix: Add changes to python script for task 7 and 10

--- 0x04-python-more_data_structures/10-best_score.py
+++ 0x04-python-more_data_structures/10-best_score.py
@@ -1,6 +1,12 @@
 #!/usr/bin/python3
 
 def best_score(a_dictionary):
-    if a_dictionary is None or len(a_dictionary) == 0:
+    if not isinstance(a_dictionary, dict) or len(a_dictionary) == 0:
         return None
-    return max(list(a_dictionary.values()))
+    mkey = list(a_dictionary.keys)[0]
+    mvalue = a_dictionary[mkey]
+    for k, v in a_dictionary.items():
+        if v > mvalue:
+            mkey = k
+            mvalue = v
+    return mkey
